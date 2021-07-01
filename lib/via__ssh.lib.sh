3cc080dd36c857423e12575f7f84392b13e35201
TsT <tst2005@gmail.com>
2021-07-01 19:14:41 +0200

    via__ssh auto detect and use of -n

--- lib/via__ssh.lib.sh
+++ lib/via__ssh.lib.sh
@@ -11,5 +11,8 @@ via__ssh() {
 		(true)  set -- -o 'ControlMaster auto' -o 'ControlPath "~/.ssh/.master-%r@%h:%p"' -o 'ControlPersist 60s' "$@";;
 		(false) set -- -o 'ControlMaster no'   -o 'ControlPath none'                      -o 'ControlPersist  0s' "$@";;
 	esac
+	if [ -t 0 ]; then
+		set -- -n "$@"
+	fi
 	ssh $(hostname_with_ssh_options "$target" | grep '' || echo "$target") "$@"
 }
