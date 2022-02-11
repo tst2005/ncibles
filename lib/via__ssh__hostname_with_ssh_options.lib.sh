948c0147d59266d1f560865e9076a2ac5389e223
TsT <tst2005@gmail.com>
2022-02-11 11:16:12 +0100

    support use without NCIBLES_ETCDIR/ssh_hosts

--- lib/via__ssh__hostname_with_ssh_options.lib.sh
+++ lib/via__ssh__hostname_with_ssh_options.lib.sh
@@ -1,4 +1,6 @@
 via__ssh__hostname_with_ssh_options() {
-	grep '^'"$1"'[[:space:]]\+' "${NCIBLES_ETCDIR:-./etc}/ssh_hosts"|
-	cut -d= -f2-
+	if [ -f "${NCIBLES_ETCDIR:-./etc}/ssh_hosts" ]; then
+		grep '^'"$1"'[[:space:]]\+' "${NCIBLES_ETCDIR:-./etc}/ssh_hosts"|
+		cut -d= -f2-
+	fi
 }
